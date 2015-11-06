5c94cc7e5a7ff85522bc67f899ef498ec90111e2
Boris Brezillon <boris.brezillon@free-electrons.com>
2015-11-06 10:40:51 +0100

    mips: nand: make use of the NAND_ECCLAYOUT() macro
    The NAND_ECCLAYOUT() macro simplify the definition of static ECC layouts.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- arch/mips/jz4740/board-qi_lb60.c
+++ arch/mips/jz4740/board-qi_lb60.c
@@ -50,20 +50,14 @@ static bool is_avt2;
 #define QI_LB60_GPIO_KEYIN8		JZ_GPIO_PORTD(26)
 
 /* NAND */
-static struct nand_ecclayout qi_lb60_ecclayout_1gb = {
-	.eccbytes = 36,
-	.eccpos = {
-		6,  7,	8,  9,	10, 11, 12, 13,
-		14, 15, 16, 17, 18, 19, 20, 21,
-		22, 23, 24, 25, 26, 27, 28, 29,
-		30, 31, 32, 33, 34, 35, 36, 37,
-		38, 39, 40, 41
-	},
-	.oobfree = {
-		{ .offset = 2, .length = 4 },
-		{ .offset = 42, .length = 22 }
-	},
-};
+NAND_ECCLAYOUT(qi_lb60_ecclayout_1gb,
+	       NAND_ECCLAYOUT_ECCPOS(6, 7, 8, 9, 10, 11, 12, 13,
+				     14, 15, 16, 17, 18, 19, 20, 21,
+				     22, 23, 24, 25, 26, 27, 28, 29,
+				     30, 31, 32, 33, 34, 35, 36, 37,
+				     38, 39, 40, 41),
+	       NAND_ECCLAYOUT_OOBFREE(NAND_OOBFREE(2, 4),
+				      NAND_OOBFREE(42, 22)));
 
 /* Early prototypes of the QI LB60 had only 1GB of NAND.
  * In order to support these devices as well the partition and ecc layout is
@@ -86,24 +80,18 @@ static struct mtd_partition qi_lb60_partitions_1gb[] = {
 	},
 };
 
-static struct nand_ecclayout qi_lb60_ecclayout_2gb = {
-	.eccbytes = 72,
-	.eccpos = {
-		12, 13, 14, 15, 16, 17, 18, 19,
-		20, 21, 22, 23, 24, 25, 26, 27,
-		28, 29, 30, 31, 32, 33, 34, 35,
-		36, 37, 38, 39, 40, 41, 42, 43,
-		44, 45, 46, 47, 48, 49, 50, 51,
-		52, 53, 54, 55, 56, 57, 58, 59,
-		60, 61, 62, 63, 64, 65, 66, 67,
-		68, 69, 70, 71, 72, 73, 74, 75,
-		76, 77, 78, 79, 80, 81, 82, 83
-	},
-	.oobfree = {
-		{ .offset = 2, .length = 10 },
-		{ .offset = 84, .length = 44 },
-	},
-};
+NAND_ECCLAYOUT(qi_lb60_ecclayout_2gb,
+	       NAND_ECCLAYOUT_ECCPOS(12, 13, 14, 15, 16, 17, 18, 19,
+				     20, 21, 22, 23, 24, 25, 26, 27,
+				     28, 29, 30, 31, 32, 33, 34, 35,
+				     36, 37, 38, 39, 40, 41, 42, 43,
+				     44, 45, 46, 47, 48, 49, 50, 51,
+				     52, 53, 54, 55, 56, 57, 58, 59,
+				     60, 61, 62, 63, 64, 65, 66, 67,
+				     68, 69, 70, 71, 72, 73, 74, 75,
+				     76, 77, 78, 79, 80, 81, 82, 83),
+	       NAND_ECCLAYOUT_OOBFREE(NAND_OOBFREE(2, 10),
+				      NAND_OOBFREE(84, 44)));
 
 static struct mtd_partition qi_lb60_partitions_2gb[] = {
 	{
