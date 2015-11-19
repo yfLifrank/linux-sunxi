e7e90599b840a740d5eeb4a90815c707ac7f7c25
Maxime Ripard <maxime.ripard@free-electrons.com>
2015-11-19 17:20:17 +0100

    fixup! drm: sun4i: Add composite output

--- drivers/gpu/drm/sun4i/sun4i_drv.c
+++ drivers/gpu/drm/sun4i/sun4i_drv.c
@@ -140,7 +140,7 @@ static int sun4i_drv_load(struct drm_device *drm, unsigned long flags)
 	if (ret) {
 		dev_warn(drm->dev, "Couldn't create our TV output\n");
 		if (ret == -EPROBE_DEFER)
-			return ret
+			return ret;
 	}
 
 	/* Create our framebuffer */
